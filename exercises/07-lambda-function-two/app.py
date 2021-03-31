f5f0b0f01c4190a27c3be33783d504fe9eb3963d
Moises Castillo <mcastillo093@gmail.com>
2021-03-31 01:19:42 +0000

    exercises completed

--- exercises/07-lambda-function-two/app.py
+++ exercises/07-lambda-function-two/app.py
@@ -1,5 +1,5 @@
 
-
+rapid = lambda text: text[:-1]   
 
 # From this line above, plese do not change code below
 print(rapid("bob")) #should print bo
--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,5 +1,7 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
 
+def sort_names(ar):
+    return sorted(ar)
 
 print(sort_names(names))
